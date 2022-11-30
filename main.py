9bb5ed8dd368f80b590873d44875c81ac1359cb6
K Lab <kaikairobot@hotmail.com>
2022-11-30 20:17:27 +0900

    1.1.3

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-#from itertools import count
 import json
 from requests_oauthlib import OAuth1Session
 import time
 from time import sleep
-import traceback
 import tweepy
 import requests
 import schedule
@@ -14,18 +12,27 @@ import tweets
 import global_value as g
 import os
 
+# To set your enviornment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
 consumer_key = os.environ['consumer_key']
 consumer_secret = os.environ['consumer_secret']
 access_token = os.environ['access_token']
 access_token_secret = os.environ['access_token_secret']
 bearer_token = os.environ['bearer_token']
+API_KEY_mebo = os.environ['bearer_token']
+agent_id = os.environ['bearer_token']
 
 Client = tweepy.Client(bearer_token, consumer_key, consumer_secret, access_token, access_token_secret)
 
 def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
     r.headers["Authorization"] = f"Bearer {bearer_token}"
     r.headers["User-Agent"] = "v2FilteredStreamPython"
     return r
+
 
 def get_rules():
     response = requests.get(
@@ -37,6 +44,7 @@ def get_rules():
         )
     print(json.dumps(response.json()))
     return response.json()
+
 
 def delete_all_rules(rules):
     if rules is None or "data" not in rules:
@@ -58,14 +66,12 @@ def delete_all_rules(rules):
     print(json.dumps(response.json()))
 
 
-
 def set_rules(delete):
-    rules = [
-        {
-            "value":"to:k20824387" ##←ここを書き換えるよ
-        }
+    # You can adjust the rules if needed
+    sample_rules = [
+        {"value": "to:k20824387"}
     ]
-    payload = {"add": rules}
+    payload = {"add": sample_rules}
     response = requests.post(
         "https://api.twitter.com/2/tweets/search/stream/rules",
         auth=bearer_oauth,
@@ -78,86 +84,61 @@ def set_rules(delete):
     print(json.dumps(response.json()))
 
 
-def get_stream(headers):
-    run = 1
-    while run:
-        try:
-            with requests.get(
-                "https://api.twitter.com/2/tweets/search/stream?expansions=author_id&user.fields=name", auth=bearer_oauth, stream=True,
-            ) as response:
-                print(response.status_code)
-                time.sleep(1)
-                if response.status_code != 200:
-                    raise Exception(
-                        "Cannot get stream (HTTP {}): {}".format(
-                            response.status_code, response.text
-                        )
-                    )
-                for response_line in response.iter_lines():
-                    if response_line:
-                        json_response = json.loads(response_line)
-                        tweet_id = json_response["data"]["id"] #ツイートID
-                        reply_text=json_response["data"]["text"] #相手の送ってきた内容
-                        user_id=json_response["data"]["author_id"]#ユーザーID
-                        user_name=json_response["includes"]["users"]#ユーザーネーム取得
-                        user_name=user_name[0]["name"]
+def get_stream(set):
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream?expansions=author_id&user.fields=name", auth=bearer_oauth, stream=True,
+    )
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get stream (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    for response_line in response.iter_lines():
+        if response_line:
+            json_response = json.loads(response_line)
+            #print(json.dumps(json_response, indent=4, sort_keys=True))
+            
+            tweet_id = json_response["data"]["id"] #ツイートID
+            reply_text=json_response["data"]["text"] #相手の送ってきた内容
+            user_id=json_response["data"]["author_id"]#ユーザーID
+            user_name=json_response["includes"]["users"]#ユーザーネーム取得
+            user_name=user_name[0]["name"]
 
-                        print(user_name)
+            print(user_name)
 
-                        reply_text=re.sub(r'@([A-Za-z0-9_]+)', "", reply_text)
+            reply_text=re.sub(r'@([A-Za-z0-9_]+)', "", reply_text)
+
+            headers_mebo = {'Content-Type': 'application/json'}
                         
-                        headers_mebo = {'Content-Type': 'application/json'}
-                        
-                        json_data = {
-                            'api_key': os.environ['API_KEY_mebo'],
-                            'agent_id': os.environ['agent_id'],
+            json_data = {
+                            'api_key': API_KEY_mebo,
+                            'agent_id': agent_id,
                             'utterance': reply_text,
                             'uid': 'mebo.ai_' + str(user_id),
                         }
 
-                        response = requests.post('https://api-mebo.dev/api', headers=headers_mebo, data=json.dumps(json_data))
-                        
-                        res_data = response.json()
-                        replay = response.text
-                        replay = json.loads(replay)
-                        replay = replay['bestResponse']['utterance']
+            response = requests.post('https://api-mebo.dev/api', headers=headers_mebo, data=json.dumps(json_data))
 
-                        ###ここで自分のリプライの内容を設定します
-                        text = replay
+            res_data = response.json()
+            replay = response.text
+            replay = json.loads(replay)
+            replay = replay['bestResponse']['utterance']
 
-                        print(response.status_code)
-                        print(response.text)
-                        print(user_id)
-                        print(replay)
-			
-                        print(text)
-                        Client.create_tweet(
-                            text=text,
-                            in_reply_to_tweet_id =tweet_id)
+            ###ここで自分のリプライの内容を設定します
+            text = replay
 
+            print(response.status_code)
+            print(response.text)
+            print(user_id)
+            print(replay)
 
-        except ChunkedEncodingError as chunkError:
-            print(traceback.format_exc())
-            time.sleep(6)
-            continue
-        
-        except ConnectionError as e:
-            print(traceback.format_exc())
-            run+=1
-            if run <10:
-                time.sleep(6)
-                print("再接続します",run+"回目")
-                continue
-            else:
-                run=0
-        except Exception as e:
-            # some other error occurred.. stop the loop
-            print("Stopping loop because of un-handled error")
-            print(traceback.format_exc())
-            run = 0
-	    
-class ChunkedEncodingError(Exception):
-    pass
+            print(text)
+            Client.create_tweet(
+                text=text,
+                in_reply_to_tweet_id =tweet_id)
+
 
 def tweet1():
     tweets.tweet()
@@ -211,6 +192,7 @@ def main():
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
     executor.submit(schedule1)
     executor.submit(get_stream(set))
+
 
 if __name__ == "__main__":
     main()
